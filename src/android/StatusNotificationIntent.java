b408bd0e129677abeaabf785b69b76da88439fe3
rstets <romko.stets@gmail.com>
2013-03-14 17:56:53 +0200

    done.

--- src/android/StatusNotificationIntent.java
+++ src/android/StatusNotificationIntent.java
@@ -8,7 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 
-//import com.my.app.R;
+import $PACKAGE_NAME.R;
 
 public class StatusNotificationIntent {
     public static Notification buildNotification( Context context, CharSequence tag, CharSequence contentTitle, CharSequence contentText, int flag ) {
